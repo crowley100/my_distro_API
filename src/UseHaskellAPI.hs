893135a2175ba4920d34646a7661f24427f912df
Lorcan reidy <loreidy@tcd.ie>
2017-02-11 17:26:52 +0000

    fixed dynamic api request

--- src/UseHaskellAPI.hs
+++ src/UseHaskellAPI.hs
@@ -88,23 +88,23 @@ toResponseData :: PubKeyInfo -> [ResponseData]
 toResponseData msg@(PubKeyInfo strKey strN strE)=((ResponseData $ strKey):(ResponseData $ strN):(ResponseData $ strE):[])
 
 -- Service information begins --
-defaultHost = "172.17.0.15" -- temporary ip to work with tcd proxy
+--defaultHost = "172.17.0.15" -- temporary ip to work with tcd proxy
 
 -- parses ip from ifconfig
---defaultHost :: IO String
---defaultHost = do
---  (_, Just hout, _, _) <- createProcess (proc "hostname" ["-I"]){ std_out = CreatePipe }
---  getHost <- hGetContents hout
---  return $ head $ words getHost
+defaultHost :: IO String
+defaultHost = do
+  (_, Just hout, _, _) <- createProcess (proc "hostname" ["-I"]){ std_out = CreatePipe }
+  (_, Just hout, _, _) <- createProcess (shell "/sbin/ip route|awk '/default/ { print $3 }'" ){ std_out = CreatePipe }
+  getHost <- hGetContents hout
+  return getHost
 
 servDoCall f p = (SC.runClientM f =<< servEnv p)
 
 servEnv :: Int -> IO SC.ClientEnv
 servEnv p = do
   man <- newManager defaultManagerSettings
-  --h <- defaultHost
-  --putStrLn h
-  return (SC.ClientEnv man (SC.BaseUrl SC.Http defaultHost p ""))
+  h <- defaultHost
+  return (SC.ClientEnv man (SC.BaseUrl SC.Http h p ""))
 
 -- possibly convert ports to strings...
 fs1IP = defaultHost
